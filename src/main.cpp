a4a306ec2d21e56181f2a8e643da48f19c68f780
Analysis Box <Analysis Box@DESKTOP-SACS6P6>
2023-03-13 03:32:02 +0000

    Add argument to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <argparse/argparse.hpp>
 
 #include <Windows.h>
 #include <inttypes.h>
@@ -9,9 +10,19 @@
 #include <memory>
 #include <stack>
 
-int main() {
-    std::string fileName = "C:\\Users\\Analysis Box\\Desktop\\malware "
-                           "collection\\vmprotect\\FilterSecurity_new.dll";
-    fmt::print("Hello World: {}\n", fileName);
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser program("Nys");
+    program.add_argument("--file").required().help("Input a file to analyse");
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error &err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    std::string file_name = program.get<std::string>("--file");
+    fmt::print("Hello World: {}\n", file_name);
     return 0;
 }
